02c059a633839609d6af7be1e06071a3b86598ef
zd <1298698045@qq.com>
2020-11-27 18:26:31 +0800

    注释app.vue代码

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script>
+const updateManager = wx.getUpdateManager()
 export default {
   data(){
     return {
@@ -21,7 +22,27 @@ export default {
      * 百度：mpvue === swan, mpvuePlatform === 'swan'
      * 支付宝(蚂蚁)：mpvue === my, mpvuePlatform === 'my'
      */
-    
+    // updateManager.onCheckForUpdate(function (res) {
+    //   // 请求完新版本信息的回调
+    //   console.log(res.hasUpdate)
+    // })
+
+    // updateManager.onUpdateReady(function () {
+    //   wx.showModal({
+    //     title: '更新提示',
+    //     content: '新版本已经准备好，是否重启应用？',
+    //     success(res) {
+    //       if (res.confirm) {
+    //         // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+    //         updateManager.applyUpdate()
+    //       }
+    //     }
+    //   })
+    // })
+
+    // updateManager.onUpdateFailed(function () {
+    //   // 新版本下载失败
+    // })
     wx.getSystemInfo({
         success: (res) => {
             console.log(res)
