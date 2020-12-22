38e72f52d1ee3b804810b8fee625b3080bee6a7e
zd <1298698045@qq.com>
2020-12-22 19:52:27 +0800

    发起流程添加请假余额判断

--- src/pages/hr/holidayAccount/index.vue
+++ src/pages/hr/holidayAccount/index.vue
@@ -35,13 +35,13 @@ export default {
   data(){
     return {
       listData:[
-        {"code":"01","text":"text1","type":"type1"},
-        {"code":"02","text":"text2","type":"type2"},
-        {"code":"03","text":"text3","type":"type3"},
-        {"code":"04","text":"text4","type":"type4"},
-        {"code":"05","text":"text5","type":"type5"},
-        {"code":"06","text":"text6","type":"type6"},
-        {"code":"07","text":"text7","type":"type7"}
+        // {"code":"01","text":"text1","type":"type1"},
+        // {"code":"02","text":"text2","type":"type2"},
+        // {"code":"03","text":"text3","type":"type3"},
+        // {"code":"04","text":"text4","type":"type4"},
+        // {"code":"05","text":"text5","type":"type5"},
+        // {"code":"06","text":"text6","type":"type6"},
+        // {"code":"07","text":"text7","type":"type7"}
       ]
     }
   },
@@ -58,6 +58,7 @@ export default {
     }
   },
   onLoad(){
+    Object.assign(this.data,this.$options.data());
     this.getQuery();
   },
   methods:{
