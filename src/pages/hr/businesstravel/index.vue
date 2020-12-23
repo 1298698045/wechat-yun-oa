07a816cdc47d11f7a4ccf297640a5bb6ca6c997f
zd <1298698045@qq.com>
2020-12-23 19:16:30 +0800

    表单加查看数据列表

--- src/pages/hr/businesstravel/index.vue
+++ src/pages/hr/businesstravel/index.vue
@@ -1,202 +1,233 @@
 <template>
   <div class="wrap">
-    <div class="content">
-      <div class="row">
-        <p class="label">
-          出差人员
-          <span>*</span>
-        </p>
-        <p class="name">
-          <input
-            type="text"
-            placeholder-style="text-align:right;color: #ababab;"
-            selection-end="-1"
-            :placeholder="'请输入申请人姓名'"
-          />
-        </p>
-      </div>
-      <div class="row">
-        <p class="label">
-          所属部门
-          <span>*</span>
-        </p>
-        <p class="name">
-          <input
-            type="text"
-            placeholder-style="text-align:right;color: #ababab;"
-            selection-end="-1"
-            :placeholder="'请输入申请部门'"
-          />
-        </p>
-      </div>
-      <picker
-        @change="pickerStartTime"
-        mode="multiSelector"
-        :value="multiIndex"
-        :range="newMultiArray"
-      >
+    <div class="navTabs">
+      <van-tabs :active="current" color="#3399ff" @change="onChange">
+        <van-tab title="发起提交"></van-tab>
+        <van-tab title="查看数据"></van-tab>
+      </van-tabs>
+    </div>
+    <div class="container" v-if="current==0">
+      <div class="content">
         <div class="row">
           <p class="label">
-            开始时间
+            出差人员
             <span>*</span>
           </p>
           <p class="name">
-            <span>{{startTime}}</span>
-            <i-icon type="enter" color="#cccccc" />
+            <input
+              type="text"
+              placeholder-style="text-align:right;color: #ababab;"
+              selection-end="-1"
+              :placeholder="'请输入申请人姓名'"
+            />
           </p>
         </div>
-      </picker>
-      <picker
-        @change="pickerEndTime"
-        mode="multiSelector"
-        :value="endMultiIndex"
-        :range="newMultiArray"
-      >
         <div class="row">
           <p class="label">
-            截止时间
+            所属部门
             <span>*</span>
           </p>
           <p class="name">
-            <span>{{endTime}}</span>
-            <i-icon type="enter" color="#cccccc" />
+            <input
+              type="text"
+              placeholder-style="text-align:right;color: #ababab;"
+              selection-end="-1"
+              :placeholder="'请输入申请部门'"
+            />
           </p>
         </div>
-      </picker>
-      <div class="row">
-        <p class="label">
-          出发地点
-          <span>*</span>
-        </p>
-        <p class="name">
-          <input
-            type="text"
-            placeholder-style="text-align:right;color: #ababab;"
-            selection-end="-1"
-            placeholder="请输入出发地点"
-            v-model="address"
-          />
-          <i-icon type="coordinates" color="#999999" size="20" @click="getChooseLocation" />
-        </p>
-      </div>
-      <div class="row">
-        <p class="label">
-          到达地点
-          <span>*</span>
-        </p>
-        <p class="name">
-          <input
-            type="text"
-            placeholder-style="text-align:right;color: #ababab;"
-            selection-end="-1"
-            placeholder="请输入到达地点"
-            v-model="address"
-          />
-          <i-icon type="coordinates" color="#999999" size="20" @click="getChooseLocation" />
-        </p>
-      </div>
-      <div class="row">
-        <p class="label">
-          预计出差天数
-          <span>*</span>
-        </p>
-        <p class="name">
-          <input
-            type="text"
-            placeholder-style="text-align:right;color: #ababab;"
-            selection-end="-1"
-            :placeholder="'请输入预计出差天数'"
-          />
-        </p>
-      </div>
-      <div class="row">
-        <p class="label">
-          交通工具
-          <span>*</span>
-        </p>
-        <p class="name">
-          <input
-            type="text"
-            placeholder-style="text-align:right;color: #ababab;"
-            selection-end="-1"
-            :placeholder="'请输入交通工具'"
-          />
-        </p>
-      </div>
-      <div class="row">
-        <p class="label">
-          预计交通费用
-          <span>*</span>
-        </p>
-        <p class="name">
-          <input
-            type="text"
-            placeholder-style="text-align:right;color: #ababab;"
-            selection-end="-1"
-            :placeholder="'请输入预计交通费用'"
-          />
-        </p>
-      </div>
-        <div class="row">
+        <picker
+          @change="pickerStartTime"
+          mode="multiSelector"
+          :value="multiIndex"
+          :range="newMultiArray"
+        >
+          <div class="row">
             <p class="label">
-            出差客户
-            <span>*</span>
+              开始时间
+              <span>*</span>
             </p>
             <p class="name">
-            <input
-                type="text"
-                placeholder-style="text-align:right;color: #ababab;"
-                selection-end="-1"
-                :placeholder="'请输入出差客户'"
-            />
+              <span>{{startTime}}</span>
+              <i-icon type="enter" color="#cccccc" />
             </p>
+          </div>
+        </picker>
+        <picker
+          @change="pickerEndTime"
+          mode="multiSelector"
+          :value="endMultiIndex"
+          :range="newMultiArray"
+        >
+          <div class="row">
+            <p class="label">
+              截止时间
+              <span>*</span>
+            </p>
+            <p class="name">
+              <span>{{endTime}}</span>
+              <i-icon type="enter" color="#cccccc" />
+            </p>
+          </div>
+        </picker>
+        <div class="row">
+          <p class="label">
+            出发地点
+            <span>*</span>
+          </p>
+          <p class="name">
+            <input
+              type="text"
+              placeholder-style="text-align:right;color: #ababab;"
+              selection-end="-1"
+              placeholder="请输入出发地点"
+              v-model="address"
+            />
+            <i-icon type="coordinates" color="#999999" size="20" @click="getChooseLocation" />
+          </p>
         </div>
-      
-      <!-- <div class="rowBottom">
-        <p>
-          根据排班自动计算时长
-          <span>查看明细</span>
-        </p>
-      </div> -->
-    </div>
-    <div class="leaveComment">
-      <p>
-        行程明细
-        <span>*</span>
-      </p>
-      <div class="box">
-        <textarea name id cols="30" rows="10"></textarea>
-      </div>
-    </div>
-    <div class="leaveComment">
-      <p>
-        备注
-        <span>*</span>
-      </p>
-      <div class="box">
-        <textarea name id cols="30" rows="10"></textarea>
-      </div>
-    </div>
-    <div class="imgContent" v-if="false">
-      <div class="head" @click="handleSelPhoto">
-        <p>图片</p>
-        <p>
-          <i class="iconfont icon-tupian"></i>
-        </p>
-      </div>
-      <div class="cont" v-if="imgList!=''">
-        <p class="imgWrap" v-for="(item,index) in imgList" :key="index">
-          <img :src="item" alt />
-          <span class="close" @click="getCloseImg(index)">
-            <i-icon type="close" color="#fff" size="12" />
-          </span>
-        </p>
-      </div>
-    </div>
-    <div class="footer" :class="{'bottomActive':isModelmes,'footImt':!isModelmes}">
-        <div class="btn">
-            <van-button type="info" block @click="getSubmit">提交</van-button>
+        <div class="row">
+          <p class="label">
+            到达地点
+            <span>*</span>
+          </p>
+          <p class="name">
+            <input
+              type="text"
+              placeholder-style="text-align:right;color: #ababab;"
+              selection-end="-1"
+              placeholder="请输入到达地点"
+              v-model="address"
+            />
+            <i-icon type="coordinates" color="#999999" size="20" @click="getChooseLocation" />
+          </p>
         </div>
+        <div class="row">
+          <p class="label">
+            预计出差天数
+            <span>*</span>
+          </p>
+          <p class="name">
+            <input
+              type="text"
+              placeholder-style="text-align:right;color: #ababab;"
+              selection-end="-1"
+              :placeholder="'请输入预计出差天数'"
+            />
+          </p>
+        </div>
+        <div class="row">
+          <p class="label">
+            交通工具
+            <span>*</span>
+          </p>
+          <p class="name">
+            <input
+              type="text"
+              placeholder-style="text-align:right;color: #ababab;"
+              selection-end="-1"
+              :placeholder="'请输入交通工具'"
+            />
+          </p>
+        </div>
+        <div class="row">
+          <p class="label">
+            预计交通费用
+            <span>*</span>
+          </p>
+          <p class="name">
+            <input
+              type="text"
+              placeholder-style="text-align:right;color: #ababab;"
+              selection-end="-1"
+              :placeholder="'请输入预计交通费用'"
+            />
+          </p>
+        </div>
+          <div class="row">
+              <p class="label">
+              出差客户
+              <span>*</span>
+              </p>
+              <p class="name">
+              <input
+                  type="text"
+                  placeholder-style="text-align:right;color: #ababab;"
+                  selection-end="-1"
+                  :placeholder="'请输入出差客户'"
+              />
+              </p>
+          </div>
+        
+        <!-- <div class="rowBottom">
+          <p>
+            根据排班自动计算时长
+            <span>查看明细</span>
+          </p>
+        </div> -->
+      </div>
+      <div class="leaveComment">
+        <p>
+          行程明细
+          <span>*</span>
+        </p>
+        <div class="box">
+          <textarea name id cols="30" rows="10"></textarea>
+        </div>
+      </div>
+      <div class="leaveComment">
+        <p>
+          备注
+          <span>*</span>
+        </p>
+        <div class="box">
+          <textarea name id cols="30" rows="10"></textarea>
+        </div>
+      </div>
+      <div class="imgContent" v-if="false">
+        <div class="head" @click="handleSelPhoto">
+          <p>图片</p>
+          <p>
+            <i class="iconfont icon-tupian"></i>
+          </p>
+        </div>
+        <div class="cont" v-if="imgList!=''">
+          <p class="imgWrap" v-for="(item,index) in imgList" :key="index">
+            <img :src="item" alt />
+            <span class="close" @click="getCloseImg(index)">
+              <i-icon type="close" color="#fff" size="12" />
+            </span>
+          </p>
+        </div>
+      </div>
+      <div class="footer" :class="{'bottomActive':isModelmes,'footImt':!isModelmes}">
+          <div class="btn">
+              <van-button type="info" block @click="getSubmit">提交</van-button>
+          </div>
+      </div>
+    </div>
+    <div class="dataWrap" v-if="current==1">
+      <div class="search">
+        <van-search :value="value" placeholder="搜索标题、编号、正文内容" />
+      </div>
+      <div class="contentList">
+        <div class="box_wrap">
+          <div class="row">
+            <p class="title">XX提交的请假</p>
+            <div class="time">2020-10-10</div>
+          </div>
+          <p class="desc">请假类型：年假</p>
+          <p class="desc">开始时间：2002-10-10 上午</p>
+          <p class="desc">结束时间：2002-10-10 下午</p>
+          <div class="tagFot">
+            <p class="tag">
+              <span>测试</span>
+              由XX提交
+            </p>
+            <p class="status">XXX处理中</p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -261,7 +292,8 @@ export default {
       startTime: "",
       endTime: "",
       imgList: [],
-      address:""
+      address:"",
+      current:0
     };
   },
   computed: {
@@ -355,6 +387,9 @@ export default {
     this.getCurrent();
   },
   methods: {
+    onChange(e){
+      this.current = e.mp.detail.index;
+    },
     getCurrent(){
         let date = new Date(this.time.replace(/-/g,'/'));
         let years = date.getFullYear();
@@ -474,6 +509,9 @@ export default {
 @import '../../../../static/css/icon.css';
 .wrap {
     padding-bottom: 100px;
+    .navTabs{
+      border-bottom: 1rpx solid #e2e3e5;
+    }
   .rowWrap {
     display: flex;
     justify-content: space-between;
@@ -629,5 +667,60 @@ export default {
             padding: 20rpx;
         }
     }
+    .dataWrap{
+    .contentList{
+      padding: 0 20rpx;
+      .box_wrap{
+        width: 100%;
+        height: auto;
+        background: #fff;
+        box-shadow: 0 2rpx 6rpx 0 rgba(0,0,0,.06);
+        margin-top: 20rpx;
+        padding: 25rpx 20rpx;
+        border-radius: 10rpx;
+        .row{
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          padding-bottom: 15rpx;
+          .title{
+
+          }
+          .time{
+            font-size: 20rpx;
+            color: #ababab;
+          }
+        }
+        .desc{
+          font-size: 24rpx;
+          color: #ababab;
+        }
+        .tagFot{
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          padding-top: 15rpx;
+          .tag{
+            display: flex;
+            align-items: center;
+            span{
+              display: inline-block;
+              width: 40rpx;
+              height: 40rpx;
+              line-height: 40rpx;
+              text-align: center;
+              background: #3399ff;
+              color: #fff;
+              font-size: 14rpx;
+              margin-right: 10rpx;
+            }
+          }
+          .status{
+            color: #f09951;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
--- src/pages/hr/goOut/index.vue
+++ src/pages/hr/goOut/index.vue
@@ -1,112 +1,141 @@
 <template>
   <div class="wrap">
-    <div class="content">
-      <div class="row">
-        <p class="label">
-          外出人员
-          <span>*</span>
-        </p>
-        <p class="name">
-          <input
-            type="text"
-            placeholder-style="text-align:right;color: #ababab;"
-            selection-end="-1"
-            :placeholder="'请输入外出人员'"
-          />
-        </p>
-      </div>
-      <div class="row">
-        <p class="label">
-          所属部门
-          <span>*</span>
-        </p>
-        <p class="name">
-          <input
-            type="text"
-            placeholder-style="text-align:right;color: #ababab;"
-            selection-end="-1"
-            :placeholder="'请输入申请部门'"
-          />
-        </p>
-      </div>
-      <picker
-        @change="pickerStartTime"
-        mode="multiSelector"
-        :value="multiIndex"
-        :range="newMultiArray"
-      >
+    <van-tabs :active="current" color="#3399ff" @change="onChange">
+      <van-tab title="发起提交"></van-tab>
+      <van-tab title="查看数据"></van-tab>
+    </van-tabs>
+    <div class="container" v-if="current==0">
+      <div class="content">
         <div class="row">
           <p class="label">
-            开始时间
+            外出人员
             <span>*</span>
           </p>
           <p class="name">
-            <span>{{startTime}}</span>
-            <i-icon type="enter" color="#cccccc" />
+            <input
+              type="text"
+              placeholder-style="text-align:right;color: #ababab;"
+              selection-end="-1"
+              :placeholder="'请输入外出人员'"
+            />
           </p>
         </div>
-      </picker>
-      <picker
-        @change="pickerEndTime"
-        mode="multiSelector"
-        :value="endMultiIndex"
-        :range="newMultiArray"
-      >
         <div class="row">
           <p class="label">
-            截止时间
+            所属部门
             <span>*</span>
           </p>
           <p class="name">
-            <span>{{endTime}}</span>
-            <i-icon type="enter" color="#cccccc" />
+            <input
+              type="text"
+              placeholder-style="text-align:right;color: #ababab;"
+              selection-end="-1"
+              :placeholder="'请输入申请部门'"
+            />
           </p>
         </div>
-      </picker>
-      <div class="row">
-        <p class="label">
-          时长
-          <span>*</span>
-        </p>
-        <p class="name">
-          <input
-            type="text"
-            placeholder-style="text-align:right;color: #ababab;"
-            selection-end="-1"
-            :placeholder="'请输入时长'"
-          />
-        </p>
+        <picker
+          @change="pickerStartTime"
+          mode="multiSelector"
+          :value="multiIndex"
+          :range="newMultiArray"
+        >
+          <div class="row">
+            <p class="label">
+              开始时间
+              <span>*</span>
+            </p>
+            <p class="name">
+              <span>{{startTime}}</span>
+              <i-icon type="enter" color="#cccccc" />
+            </p>
+          </div>
+        </picker>
+        <picker
+          @change="pickerEndTime"
+          mode="multiSelector"
+          :value="endMultiIndex"
+          :range="newMultiArray"
+        >
+          <div class="row">
+            <p class="label">
+              截止时间
+              <span>*</span>
+            </p>
+            <p class="name">
+              <span>{{endTime}}</span>
+              <i-icon type="enter" color="#cccccc" />
+            </p>
+          </div>
+        </picker>
+        <div class="row">
+          <p class="label">
+            时长
+            <span>*</span>
+          </p>
+          <p class="name">
+            <input
+              type="text"
+              placeholder-style="text-align:right;color: #ababab;"
+              selection-end="-1"
+              :placeholder="'请输入时长'"
+            />
+          </p>
+        </div>
       </div>
-    </div>
-    <div class="leaveComment">
-      <p>
-        外出事由
-        <span>*</span>
-      </p>
-      <div class="box">
-        <textarea name id cols="30" rows="10"></textarea>
-      </div>
-    </div>
-    <div class="imgContent">
-      <div class="head" @click="handleSelPhoto">
-        <p>图片</p>
+      <div class="leaveComment">
         <p>
-          <i class="iconfont icon-tupian"></i>
+          外出事由
+          <span>*</span>
         </p>
+        <div class="box">
+          <textarea name id cols="30" rows="10"></textarea>
+        </div>
       </div>
-      <div class="cont" v-if="imgList!=''">
-        <p class="imgWrap" v-for="(item,index) in imgList" :key="index">
-          <img :src="item" alt />
-          <span class="close" @click="getCloseImg(index)">
-            <i-icon type="close" color="#fff" size="12" />
-          </span>
-        </p>
+      <div class="imgContent">
+        <div class="head" @click="handleSelPhoto">
+          <p>图片</p>
+          <p>
+            <i class="iconfont icon-tupian"></i>
+          </p>
+        </div>
+        <div class="cont" v-if="imgList!=''">
+          <p class="imgWrap" v-for="(item,index) in imgList" :key="index">
+            <img :src="item" alt />
+            <span class="close" @click="getCloseImg(index)">
+              <i-icon type="close" color="#fff" size="12" />
+            </span>
+          </p>
+        </div>
+      </div>
+      <div class="footer" :class="{'bottomActive':isModelmes,'footImt':!isModelmes}">
+          <div class="btn">
+              <van-button type="info" block @click="getSubmit">提交</van-button>
+          </div>
       </div>
     </div>
-    <div class="footer" :class="{'bottomActive':isModelmes,'footImt':!isModelmes}">
-        <div class="btn">
-            <van-button type="info" block @click="getSubmit">提交</van-button>
+    <div class="dataWrap" v-if="current==1">
+      <div class="search">
+        <van-search :value="value" placeholder="搜索标题、编号、正文内容" />
+      </div>
+      <div class="contentList">
+        <div class="box_wrap">
+          <div class="row">
+            <p class="title">XX提交的请假</p>
+            <div class="time">2020-10-10</div>
+          </div>
+          <p class="desc">请假类型：年假</p>
+          <p class="desc">开始时间：2002-10-10 上午</p>
+          <p class="desc">结束时间：2002-10-10 下午</p>
+          <div class="tagFot">
+            <p class="tag">
+              <span>测试</span>
+              由XX提交
+            </p>
+            <p class="status">XXX处理中</p>
+          </div>
         </div>
+      </div>
     </div>
   </div>
 </template>
@@ -171,7 +200,8 @@ export default {
       startTime: "",
       endTime: "",
       imgList: [],
-      address:""
+      address:"",
+      current:0
     };
   },
   computed: {
@@ -265,6 +295,9 @@ export default {
     this.getCurrent();
   },
   methods: {
+    onChange(e){
+      this.current = e.mp.detail.index;
+    },
     getCurrent(){
         let date = new Date(this.time.replace(/-/g,'/'));
         let years = date.getFullYear();
@@ -539,5 +572,60 @@ export default {
             padding: 20rpx;
         }
     }
+    .dataWrap{
+    .contentList{
+      padding: 0 20rpx;
+      .box_wrap{
+        width: 100%;
+        height: auto;
+        background: #fff;
+        box-shadow: 0 2rpx 6rpx 0 rgba(0,0,0,.06);
+        margin-top: 20rpx;
+        padding: 25rpx 20rpx;
+        border-radius: 10rpx;
+        .row{
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          padding-bottom: 15rpx;
+          .title{
+
+          }
+          .time{
+            font-size: 20rpx;
+            color: #ababab;
+          }
+        }
+        .desc{
+          font-size: 24rpx;
+          color: #ababab;
+        }
+        .tagFot{
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          padding-top: 15rpx;
+          .tag{
+            display: flex;
+            align-items: center;
+            span{
+              display: inline-block;
+              width: 40rpx;
+              height: 40rpx;
+              line-height: 40rpx;
+              text-align: center;
+              background: #3399ff;
+              color: #fff;
+              font-size: 14rpx;
+              margin-right: 10rpx;
+            }
+          }
+          .status{
+            color: #f09951;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
